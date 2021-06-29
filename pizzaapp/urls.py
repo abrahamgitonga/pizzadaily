1774c0826f1f0787bf39b794c7ce63d26cc3c89d
abraham <abrahamgitonga98@gmail.com>
2021-06-29 17:22:21 +0300

    first commit

--- pizzaapp/urls.py
+++ pizzaapp/urls.py
@@ -1,10 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import adminloginview,adminhomepageview,authenticateadmin
+from .views import adminloginview,adminhomepageview,authenticateadmin, logoutadmin,addpizza,deletepizza,homepageview
 urlpatterns = [
     path('admin/', adminloginview,name = 'adminloginpage'),
     path('adminauthenticate/',authenticateadmin),
     path('admin/homepage',adminhomepageview,name = 'adminhomepage'),
+    path('adminlogout/',logoutadmin),
+    path('addpizza/',addpizza),
+    path('deletepizza/<int:pizzapk>/',deletepizza),
+    path('',homepageview)
 
 ]
--- pizzaapp/views.py
+++ pizzaapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from .models import pizzaModel
 # Create your views here.
 def adminloginview(request):
     return render(request,"pizzaapp/adminlogin.html")
@@ -11,16 +13,40 @@ def authenticateadmin(request):
     user = authenticate(username = username,password = password)
 
     #user exists
-    if user is not None:
+    if user is not None and user.username=="admin":
         login(request,user)
         return redirect('adminhomepage')
 
     #user doesnt exist
     if user is None:
+        messages.add_message(request,messages.ERROR,"Invalid Credentials")
         return redirect('adminloginpage')
 
 def adminhomepageview(request):
-    return render(request,"pizzaapp/adminhomepage.html")
+    context = {'pizzas': pizzaModel.objects.all()}
+    return render(request,"pizzaapp/adminhomepage.html",context)
+
+def logoutadmin(request):
+    logout(request)
+    return redirect('adminloginpage')
+
+def addpizza(request):
+    #code to add pizza
+    name = request.POST['pizza']
+    price = request.POST['price']
+    pizzaModel(name = name, price = price).save()
+    return redirect('adminhomepage')
+
+def deletepizza(request,pizzapk):
+    #code to delete pizza
+    pizzaModel.objects.filter(id = pizzapk).delete()
+    return redirect('adminhomepage')
+
+def homepageview(request):
+    return render(request,"pizzaapp/homepage.html")
+
+
+
 
 
 
